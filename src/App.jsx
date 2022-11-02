bb0b9c54be63317c8b79d8938ffefac73e260af3
Genison Wibawa <wibawagenisonb@gmail.com>
2022-11-02 18:01:46 +0800

    changed methodology

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,20 @@
+import React from "react"
+import Header from "./components/Header"
+import Meme from "./components/Meme"
+/**
+ * Challenge:
+ * - Create a Meme component.
+ * - Inside the Meme component, render a styled form
+ *   with our 2 inputs and the button.
+ * - Don't worry about adding any functionality yet
+ */
 function App() {
-  return <div>a</div>
+  return (
+    <div>
+      <Header />
+      <Meme />
+    </div>
+  )
 }
 
 export default App
